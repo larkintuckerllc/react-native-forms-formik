67b53989a16ee3dfac450b00e099ca75a6b6b442
John Tucker <jtucker@appirio.com>
2018-10-17 21:00:03 -0400

    Simple

--- components/SimpleForm/index.js
+++ components/SimpleForm/index.js
@@ -1,16 +1,30 @@
 import { Formik } from 'formik';
 import React from 'react';
-import { Button, View } from 'react-native';
+import { Button, TextInput, View } from 'react-native';
+import styles from './styles';
 
 const SimpleForm = () => (
   <Formik
-    onSubmit={() => {
-      console.log('SUBMITTED');
+    onSubmit={({ firstName, lastName }) => {
+      console.log(`firstName: ${firstName}`);
+      console.log(`lastName: ${lastName}`);
     }}
     render={({
+      handleChange,
       handleSubmit,
+      values: { firstName, lastName }
     }) => (
       <View>
+        <TextInput
+          onChangeText={handleChange('firstName')}
+          style={styles.rootInput}
+          value={firstName}
+        />
+        <TextInput
+          onChangeText={handleChange('lastName')}
+          style={styles.rootInput}
+          value={lastName}
+        />
         <Button
           title="Submit Simple"
           onPress={handleSubmit}
