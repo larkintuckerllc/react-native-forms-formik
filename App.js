aa6bf917e289975035ddd2178b18929aec723b4c
John Tucker <jtucker@appirio.com>
2018-10-17 21:02:48 -0400

    Checkpoint

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, StyleSheet, View } from 'react-native';
 import HelloForm from './components/HelloForm';
+import RefactorForm from './components/RefactorForm';
 import SimpleForm from './components/SimpleForm';
 
 export default class App extends React.Component {
@@ -14,13 +15,17 @@ export default class App extends React.Component {
       <View style={styles.container}>
           <Button title="Hello Form" onPress={this.handlePressHello} />
           <Button title="Simple Form" onPress={this.handlePressSimple} />
+          <Button title="RefactorForm" onPress={this.handlePressRefactor} />
           {activeForm === 'hello' && <HelloForm />}
           {activeForm === 'simple' && <SimpleForm />}
+          {activeForm === 'refactor' && <RefactorForm />}
       </View>
     );
   }
 
   handlePressHello = () => this.setState({ activeForm: 'hello' });
+
+  handlePressRefactor = () => this.setState({ activeForm: 'refactor' });
 
   handlePressSimple = () => this.setState({ activeForm: 'simple' });
 }
---
+++ components/RefactorForm/index.js
@@ -0,0 +1,37 @@
+import { Formik } from 'formik';
+import React from 'react';
+import { Button, TextInput, View } from 'react-native';
+import styles from './styles';
+
+const RefactorForm = () => (
+  <Formik
+    onSubmit={({ firstName, lastName }) => {
+      console.log(`firstName: ${firstName}`);
+      console.log(`lastName: ${lastName}`);
+    }}
+    render={({
+      handleChange,
+      handleSubmit,
+      values: { firstName, lastName }
+    }) => (
+      <View>
+        <TextInput
+          onChangeText={handleChange('firstName')}
+          style={styles.rootInput}
+          value={firstName}
+        />
+        <TextInput
+          onChangeText={handleChange('lastName')}
+          style={styles.rootInput}
+          value={lastName}
+        />
+        <Button
+          title="Submit Simple"
+          onPress={handleSubmit}
+        />
+      </View>
+    )}
+  />
+);
+
+export default RefactorForm;
